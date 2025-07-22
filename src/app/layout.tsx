f6220c32fab877500b461830281f5ea74c577bf9
lwooden <lowell.wooden@gmail.com>
2025-07-22 07:50:08 -0400

    clerk config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
 import "./globals.css"
 import Navbar from "./components/Navbar"
+import { ClerkProvider } from "@clerk/nextjs"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,14 +25,16 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Navbar />
-        {/* TODO: Add a left sidebar */}
-        <main>{children}</main>
-      </body>
-    </html>
+    <ClerkProvider afterSignOutUrl="/login">
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <Navbar />
+          {/* TODO: Add a left sidebar */}
+          <main>{children}</main>
+        </body>
+      </html>
+    </ClerkProvider>
   )
 }
